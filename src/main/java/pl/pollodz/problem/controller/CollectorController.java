88b0d76b43e81ba9bdce96893d680e5a5d8c5c50
glogowski <temp2glogowski@gmail.com>
2018-05-05 17:06:51 +0200

    simple repo

--- src/main/java/pl/pollodz/problem/controller/CollectorController.java
+++ src/main/java/pl/pollodz/problem/controller/CollectorController.java
@@ -1,18 +1,26 @@
 package pl.pollodz.problem.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.pollodz.problem.model.device.Device;
 import pl.pollodz.problem.pojo.SimpleMeasurementsList;
+import pl.pollodz.problem.repository.DeviceRepository;
 
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @RestController
 public class CollectorController {
 
+    @Autowired
+    private DeviceRepository deviceRepository;
+
     @RequestMapping(value = "collect", method = POST)
     public String collectData(@RequestBody SimpleMeasurementsList measurementsList) {
-        System.out.println("xd");
+
+        Device device = deviceRepository.findByDeviceId("thermometer_1");
+
         return "Collection complete.";
     }
 }
---
+++ src/main/java/pl/pollodz/problem/repository/DeviceRepository.java
@@ -0,0 +1,9 @@
+package pl.pollodz.problem.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.pollodz.problem.model.device.Device;
+
+public interface DeviceRepository extends JpaRepository<Device, Long> {
+
+    Device findByDeviceId(String deviceId);
+}
