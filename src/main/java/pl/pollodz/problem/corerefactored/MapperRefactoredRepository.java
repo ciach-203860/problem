a4b30a4cfb46513cf9fea45fb80ebbd3b3809ad5
glogowski <temp96glogowski@gmail.com>
2018-05-21 21:59:49 +0200

    deleted

--- src/main/java/pl/pollodz/problem/corerefactored/MapperRefactoredRepository.java
+++
@@ -1,6 +0,0 @@
-package pl.pollodz.problem.corerefactored;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MapperRefactoredRepository extends JpaRepository<MapperRefactored, Long> {
-}
